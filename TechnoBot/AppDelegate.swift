5bfbb4c29ce63aa601f4bd8728f2183cfbc90410
Matthew Mansell <matthew.mansell14@gmail.com>
2018-02-09 17:42:08 +0000

    Initial Commit

---
+++ TechnoBot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TechnoBot
+//
+//  Created by Matthew Mansell on 09/02/2018.
+//  Copyright © 2018 MatthewMansell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TechnoBot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TechnoBot
+//
+//  Created by Matthew Mansell on 09/02/2018.
+//  Copyright © 2018 MatthewMansell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
